2f5d4f2094f670a77f36df83a63963641c44a003
ArturoRM <15040581@itdurango.edu.mx>
2018-10-16 16:33:58 -0500

    Nueva Rama
    Rama para menu H

---
+++ src/app/menu-h/menu-h.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-h',
+  templateUrl: './menu-h.component.html',
+  styleUrls: ['./menu-h.component.css']
+})
+export class MenuHComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
