6bf2334e15cd61d759a184bb993fd2f05e1bc999
MarianaVenegasFl <marianavenegas604@gmail.com>
2018-10-17 13:26:41 -0500

    Cambiar la estetica

--- src/app/verificacion-beca/verificacion-beca.component.ts
+++ src/app/verificacion-beca/verificacion-beca.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import *as $ from 'jquery'
 
 @Component({
   selector: 'app-verificacion-beca',
@@ -10,6 +11,34 @@ export class VerificacionBecaComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(menu);
+ 
+var contador = 1;
+ 
+function menu () {
+	$('.menu_bar').click(function(){
+		if (contador == 1) {
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+	});
+ 
+	// Mostramos y ocultamos submenus
+	$('.submenu').click(function(){
+		$(this).children('.children').slideToggle();
+	});
+
+	$('ol').click(function(p){
+		p.stopPropagation();
+	})
+}
   }
 
 }
