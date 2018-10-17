67e58b635824a2cf68aff5f9cf71e7d22c818a16
ArturoRM <15040581@itdurango.edu.mx>
2018-10-16 19:19:34 -0500

    Update src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -66,7 +66,7 @@ const routes:Routes = [
     EvaluacionDocenteComponent,
     AuditoriaServicioComponent,
     ListaModulosComponent,
-    MenuHComponent
+    MenuHComponent,
   ],
   imports: [
     BrowserModule,
