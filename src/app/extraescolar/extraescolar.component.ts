3960e9ce5f142096b83676355d7eb2bcb5055116
Edgar Alexis Mercado Valdez <15040440@itdurango.edu.mx>
2018-10-17 21:43:58 -0500

    cambio de tablas
    reacomodo de tablas y css , aun pendiente de arreglar modal

--- src/app/extraescolar/extraescolar.component.ts
+++ src/app/extraescolar/extraescolar.component.ts
@@ -3,44 +3,44 @@ import * as $ from 'jquery';
 
 
 @Component({
-  selector: 'app-extraescolar',
+  selector: 'app-extraescolares',
   templateUrl: './extraescolar.component.html',
   styleUrls: ['./extraescolar.component.css']
 })
 export class ExtraescolarComponent implements OnInit {
   modals = "modal";
-    constructor() {
-      this.modals = "modal";
-    }
-    func() {
-      this.modals = "visibleNo";
-    }
-    cerrar(){
-      this.modals="modal";
-    }
-    ngOnInit() {
-      $(".submenu").click(function() {
-        $(this).children("ul").slideToggle();
-
-      }
-      )
-
-      $("ul").click(function(p) {
-        p.stopPropagation();
-      })
-
-      if ($(window).width() > 720) {
-
-
-        $(".submenu").children("ul").show();
-
-
-
-      }
-      else {
-
-      }
-
-
+  constructor() {
+    this.modals = "modal";
   }
+  func() {
+    this.modals = "visibleNo";
   }
+  cerrar(){
+    this.modals="modal";
+  }
+  ngOnInit() {
+    $(".submenu").click(function() {
+      $(this).children("ul").slideToggle();
+
+    }
+    )
+
+    $("ul").click(function(p) {
+      p.stopPropagation();
+    })
+
+    if ($(window).width() > 720) {
+
+
+      $(".submenu").children("ul").show();
+
+
+
+    }
+    else {
+
+    }
+
+
+}
+}
