464444730f336d75ecf76503b9b978909c8efd1e
ArturoRM <15040581@itdurango.edu.mx>
2018-10-19 10:52:27 -0500

    Se creo rama PostSeleccionMaterias
    A peticion de kevin

---
+++ src/app/post-seleccion-materias/post-seleccion-materias.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-seleccion-materias',
+  templateUrl: './post-seleccion-materias.component.html',
+  styleUrls: ['./post-seleccion-materias.component.css']
+})
+export class PostSeleccionMateriasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
