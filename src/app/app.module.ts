029090d69ea159ca349e60f45d4059e258eb404b
ArturoRM <15040581@itdurango.edu.mx>
2018-10-19 10:47:59 -0500

    Se creo nueva rama pre-seleccion-materias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -21,6 +21,7 @@ import { ExtraescolarComponent } from './extraescolar/extraescolar.component';
 import { EvaluacionDocenteComponent } from './evaluacion-docente/evaluacion-docente.component';
 import { AuditoriaServicioComponent } from './auditoria-servicio/auditoria-servicio.component';
 import { ListaModulosComponent } from './lista-modulos/lista-modulos.component';
+import { PreSeleccionMateriasComponent } from './pre-seleccion-materias/pre-seleccion-materias.component';
 
 const routes:Routes = [
   { path: '', component: ListaModulosComponent },
@@ -40,7 +41,8 @@ const routes:Routes = [
   { path: 'gruposCargados', component: GruposCargadosComponent },
   { path: 'extraescolar', component: ExtraescolarComponent },
   { path: 'evaluacionDocente', component: EvaluacionDocenteComponent },
-  { path: 'auditoriaServicio', component: AuditoriaServicioComponent }
+  { path: 'auditoriaServicio', component: AuditoriaServicioComponent },
+  { path: 'preSeleccionMaterias', component: PreSeleccionMateriasComponent}
 ]
 
 @NgModule({
@@ -64,6 +66,7 @@ const routes:Routes = [
     EvaluacionDocenteComponent,
     AuditoriaServicioComponent,
     ListaModulosComponent,
+    PreSeleccionMateriasComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pre-seleccion-materias/pre-seleccion-materias.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pre-seleccion-materias',
+  templateUrl: './pre-seleccion-materias.component.html',
+  styleUrls: ['./pre-seleccion-materias.component.css']
+})
+export class PreSeleccionMateriasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
