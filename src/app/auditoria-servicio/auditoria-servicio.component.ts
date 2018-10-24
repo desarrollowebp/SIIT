2f90f674f21e5b2f0fe06f4c668a72939ce816cb
Emmanuel Rios <14041309@itdurango.edu.mx>
2018-10-23 19:43:53 -0500

    Creacion de tipos y preguntas

--- src/app/auditoria-servicio/auditoria-servicio.component.ts
+++ src/app/auditoria-servicio/auditoria-servicio.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-auditoria-servicio',
   templateUrl: './auditoria-servicio.component.html',
   styleUrls: ['./auditoria-servicio.component.css']
 })
 export class AuditoriaServicioComponent implements OnInit{
-
-
+	
 	constructor()	{}
-	ngOnInit() {
 
+	ngOnInit() {
 	}
   	 
 }
--- src/app/centro-info/centro-info.component.ts
+++ src/app/centro-info/centro-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Tpregunta } from '../auditoria-servicio/tipos/pregunta';
 
 @Component({
   selector: 'app-centro-info',
@@ -6,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./centro-info.component.css']
 })
 export class CentroInfoComponent implements OnInit {
+  pregunta:Tpregunta={
+      "servicio" :"", 
+      "pregunta1":"",
+      "pregunta2":"",
+      "pregunta3":"",
+      "pregunta4":"",
+      "pregunta5":"",
+      "pregunta6":"",
+      "pregunta7":"",
+      "pregunta8":"",
+      "pregunta9":"",
+  };
+
+preguntas : Tpregunta[] = [{
+    "servicio" :"Centro de Informacion",
+    "pregunta1":"1.-¿Tiene un Horario de consulta?",
+    "pregunta2":"2.-¿La informacion con la que cuenta me apoya en las asignaturas que curso?",
+    "pregunta3":"3.-¿Siempre encuentro por lo menos un ejemplar disponible de la biografia señalada en las asignaturas?",
+    "pregunta4":"4.-¿La bibliografia de la que se dispone es actualizada?",
+    "pregunta5":"5.-¿Se me proporciona atencion adecuada en caso de buscar determinado libro?",
+    "pregunta6":"6.-¿Me orientan adecuadamente para encontrar en caso de carencia, libros equivalentes al requerido?",
+    "pregunta7":"7.-¿Tienen disposicion para atenderme cuando solicito apoyo?",
+    "pregunta8":"8.-¿Me atienden en forma amable cuando solicito su apoyo?",
+    "pregunta9":"9.-¿Mantienen una relacion atenta conmigo durante mi estancia?"
+  }  
+];
 
   constructor() { }
 
