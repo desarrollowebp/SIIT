14b1e18defb448af6849c2290b8a533661c7cc51
Kevin96Wolf <15040428@@itdurango.edu.mx>
2018-10-24 23:11:56 -0500

    Faltaba JQUERY

--- src/app/avance-materia-alumno/avance-materia-alumno.component.ts
+++ src/app/avance-materia-alumno/avance-materia-alumno.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-avance-materia-alumno',
   templateUrl: './avance-materia-alumno.component.html',
