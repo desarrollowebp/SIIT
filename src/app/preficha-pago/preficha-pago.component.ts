ace1eec7d76730fd7df56cede2f91348ed40a939
juan carlos <karlosrt_cobal@hotmail.com>
2018-10-28 23:12:01 -0600

    juan carlos
    ngFor

--- src/app/preficha-pago/preficha-pago.component.ts
+++ src/app/preficha-pago/preficha-pago.component.ts
@@ -8,13 +8,24 @@ import * as $ from 'jquery';
 })
 export class PrefichaPagoComponent implements OnInit {
 
+
+  
+
   constructor() { }
+
+  
 
   ngOnInit() {
   	$('h1').click(function() {
       $(this).fadeOut()
     })
-  
-  }
 
+  }
+    importe=[{ dato:'importe a pagar:' , importe:'2450.00'} ]
+    semes=[{dato:'semestre:',semest:'9'} ]
+    numero=[{dato:'Numero de Control:',numerocontrol:'14040437'}]
+    alumno=[{dato:'Alumno:', nombre:'Abrego Salmeron Ricardo'}]
+    carrera=[{dato:'Carrera:', carre:'Ingeneria En Sistemas Computacionales'}]
+    consep=[{dato:'Consepto:', reinc:'INSCRIPCION REINGRESO'}]
+    fecha=[{dato:'Periodo',dia:'AGOSTO-DICIEMBRE/2018',}]
 }
