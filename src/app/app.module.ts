4a6e61565a3cdad048ff262683f4dcb354a1bef7
tttt1111 <41436024+tttt1111@users.noreply.github.com>
2018-10-28 18:38:11 -0600

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,11 +30,8 @@ import { EvaluacionDocenteComprobanteComponent } from './evaluacion-docente-comp
 import { AvanceMateriaAlumnoComponent } from './avance-materia-alumno/avance-materia-alumno.component';
 
 import { PostSeleccionMateriasComponent } from './post-seleccion-materias/post-seleccion-materias.component';
-<<<<<<< HEAD
 import { DatosTablaAlumnoComponent } from './datos-tabla-alumno/datos-tabla-alumno.component';
-=======
 import { TablaHorarioComponent } from './tabla-horario/tabla-horario.component';
->>>>>>> ecd77289cf14a3e1eec1e015ffe16efffaf118c3
 
 const routes:Routes = [
   { path: '', component: LoginComponent },
@@ -64,11 +61,8 @@ const routes:Routes = [
   { path: 'auditoriaServicio', component: AuditoriaServicioComponent },
   { path: 'postSeleccionMaterias', component:PostSeleccionMateriasComponent},
   { path: 'preSeleccionMaterias', component: PreSeleccionMateriasComponent},
-<<<<<<< HEAD
-  { path: 'datosTablaAlumno', component:DatosTablaAlumnoComponent}
-=======
+  { path: 'datosTablaAlumno', component:DatosTablaAlumnoComponent},
   { path: 'tablaHorario', component:TablaHorarioComponent}
->>>>>>> ecd77289cf14a3e1eec1e015ffe16efffaf118c3
 ]
 
 @NgModule({
@@ -100,11 +94,8 @@ const routes:Routes = [
     AvanceMateriaAlumnoComponent,
     PostSeleccionMateriasComponent,
     PreSeleccionMateriasComponent,
-<<<<<<< HEAD
     DatosTablaAlumnoComponent,
-=======
     TablaHorarioComponent,
->>>>>>> ecd77289cf14a3e1eec1e015ffe16efffaf118c3
   ],
   imports: [
     BrowserModule,
