5c515a92eabf8ad9841338f2f3d55d6ab3689ab7
Javier Castro <13040465@itdurango.edu.mx>
2018-10-29 23:51:02 -0600

    Se agrego la tabla alumnos(componente), se incluyo el tutor a la lista, cambio de color a negro de avance de actividades y se definieron las variables en .ts para llamarlas en el html. Fatima Lugo y Javier Castro

--- src/app/tutorias/tutorias.component.ts
+++ src/app/tutorias/tutorias.component.ts
@@ -36,5 +36,93 @@ export class TutoriasComponent implements AfterViewInit {
 
   }
 
+  tutor = [
+    { dato: 'Tutor', nombre: 'Alexander Anderson Huerta' }
+  ]
+
+  actividad1 = [
+    { dato: 'Actividad 1' }
+  ]
+
+  actividad2 = [
+    { dato: 'Actividad 2' }
+  ]
+
+  actividad3 = [
+    { dato: 'Actividad 3' }
+  ]
+
+  actividad4 = [
+    { dato: 'Actividad 4' }
+  ]
+
+  actividad5 = [
+    { dato: 'Actividad 5' }
+  ]
+
+  actividad6 = [
+    { dato: 'Actividad 6' }
+  ]
+
+  actividad7 = [
+    { dato: 'Actividad 7' }
+  ]
+
+  actividad8 = [
+    { dato: 'Actividad 8' }
+  ]
+
+  actividad9 = [
+    { dato: 'Actividad 9' }
+  ]
+
+  actividad10 = [
+    { dato: 'Actividad 10' }
+  ]
+
+  actividad11 = [
+    { dato: 'Actividad 11' }
+  ]
+
+  actividad12 = [
+    { dato: 'Actividad 12' }
+  ]
+
+  actividad13 = [
+    { dato: 'Actividad 13' }
+  ]
+
+  actividad14 = [
+    { dato: 'Actividad 14' }
+  ]
+
+  actividad15 = [
+    { dato: 'Actividad 15' }
+  ]
+
+  actividad16 = [
+    { dato: 'Actividad 16' }
+  ]
+
+  actividad17 = [
+    { dato: 'Actividad 17' }
+  ]
+
+  actividad18 = [
+    { dato: 'Actividad 18' }
+  ]
+
+  actividad19 = [
+    { dato: 'Actividad 19' }
+  ]
+
+  actividad20 = [
+    { dato: 'Actividad 20' }
+  ]
+
+  avance = [
+    { dato: 'Avance de actividades' }
+  ]
+
 }
 	
