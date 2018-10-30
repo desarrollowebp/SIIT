db4ee6c5da0cb1314178b1967bf3fbf7913cda58
MarianaVenegasFl <marianavenegas604@gmail.com>
2018-10-29 18:35:35 -0600

    Implementacion de variables

--- src/app/verificacion-beca/verificacion-beca.component.ts
+++ src/app/verificacion-beca/verificacion-beca.component.ts
@@ -10,6 +10,21 @@ export class VerificacionBecaComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
+  }
+  alumno = [
+    { dato: 'Numero de control', numerodecontrol: '14041573', nombre: 'MARIANA IDALI VENEGAS FLORES', semestre: '9', periodoescolar: 'Agosto-Diciembre/2018', promacum: '80.91'}
+  ]
+  carrera = [
+    { dato: 'carrera', carrera: 'Ing.Tics', especialidad: 'Tecnologias Web', curp: 'VEFM941118MDGNLR04', pronabes: 'si' }
+  ]
+ 
+}
+
+
+
+
+
   }
 
 }
