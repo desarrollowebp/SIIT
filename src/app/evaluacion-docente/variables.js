9d979ef816b59e5bf463d5a27a2972383c470b30
Orlando Valverde <13041580@itdurango.edu.mx>
2018-11-09 10:45:57 -0600

    Se agregaron variables

---
+++ src/app/evaluacion-docente/variables.js
@@ -0,0 +1,8 @@
+const pregunta = '?' // String
+const materia = {...rest, maestro: {_id: 'xyz'}, nombre: "mate"}
+const respuesta = 'R'
+
+const preguntas = [{ pregunta }]
+const respuestas = [{materia, respuestas: [respuesta]}]
+
+// Modifica alumno hizoEvaluacionDocente: true
