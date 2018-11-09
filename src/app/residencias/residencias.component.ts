248f51e1bb71aba791965b9e9a870ba47da937c6
KateVBR <14041418@itdurango.edu.mx>
2018-11-09 11:34:34 -0600

    Variables, donde se mandan llamar con objetos

--- src/app/residencias/residencias.component.ts
+++ src/app/residencias/residencias.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./residencias.component.css']
 })
 export class ResidenciasComponent implements OnInit {
-
+  residencia={folio:"INN",nocrtl:"14041418",nombreAlumno:"Kate Bonilla",
+carrera:"Ing. Informatica", seguimiento1:"Entregado",seguimiento2:"Entregado",seguimiento3:"Entregado",
+nombreProyecto:"Sistema de administracion",
+empresa:"SEED",asesorInt:"Gustavo Higareda",asesorExt:"Armando Ramirez",dictamen:"Aceptado",
+ portadaAnt:"Entregado", asesorInt2:"Armando Leonel",
+reporteFinal:"Entregado",revFinal:"Entregado"};
+  
   constructor() { }
 
   ngOnInit() {
