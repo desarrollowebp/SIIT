02665c1549cfcb8f8c9a00d20f6afd08d5dc5782
Kevin96Wolf <15040428@@itdurango.edu.mx>
2018-11-21 20:34:45 -0600

    Seleccion Materias
    Seleccion materias casi lista para back , variables definidas

--- src/app/seleccion-materias/seleccion-materias.component.ts
+++ src/app/seleccion-materias/seleccion-materias.component.ts
@@ -119,7 +119,7 @@ export class SeleccionMateriasComponent implements OnInit {
                                                       cadenas:["xzy","xzy"]
                                                       },
                                                       {
-                                                       codigo:"XXX", nombre:"Ejemplo",
+                                                       codigo:"AAA", nombre:"Ejemplo",
                                                        numeroCreditos:'x',
                                                        uno:0, dos:0,tres:0,cuatro:0,cinco:0,seis:0,siete:0,ocho:0,nueve:0,diez:0,
                                                        tipoEvaluacion:"Ordinario",
@@ -128,7 +128,7 @@ export class SeleccionMateriasComponent implements OnInit {
                                                       }
                                                    ],
                                                    [ {
-                                                      codigo:"XXX", nombre:"Ejemplo",
+                                                      codigo:"AAA", nombre:"Quimica",
                                                       numeroCreditos:'x',
                                                       uno:0, dos:0,tres:0,cuatro:0,cinco:0,seis:0,siete:0,ocho:0,nueve:0,diez:0,
                                                       tipoEvaluacion:"Ordinario",
@@ -136,7 +136,7 @@ export class SeleccionMateriasComponent implements OnInit {
                                                       cadenas:["xzy","xzy"]
                                                       },
                                                       {
-                                                      codigo:"XXX", nombre:"Ejemplo",
+                                                      codigo:"XY", nombre:"Ejemplo",
                                                       numeroCreditos:'x',
                                                       uno:0, dos:0,tres:0,cuatro:0,cinco:0,seis:0,siete:0,ocho:0,nueve:0,diez:0,
                                                       tipoEvaluacion:"Ordinario",
@@ -165,34 +165,22 @@ export class SeleccionMateriasComponent implements OnInit {
 ]; //dinamica
 
     maestros= [
-                 [
-                 "Martin",
-                 [
-                   {codigo:"X",Nombre:"algrebra",grupo:"X",cupo:30,horario:{lunes:"17:00",martes:"17:30",miercoles:"17:30",jueves:"17:30",viernes:"17:30",sabado:"17:30"}},
-                   {codigo:"XY",Nombre:"algrebra",grupo:"XY",cupo:30,horario:{lunes:"17:00",martes:"17:30",miercoles:"17:30",jueves:"17:30",viernes:"17:30",sabado:"17:30"}},
-                   {codigo:"XZ",Nombre:"algrebra",grupo:"XZ",cupo:30,horario:{lunes:"17:00",martes:"17:30",miercoles:"17:30",jueves:"17:30",viernes:"17:30",sabado:"17:30"}}
-                 ]
-               ],
-               [
-               "Kevin",
-               [
-                 {codigo:"X",Nombre:"algrebra",grupo:"X",cupo:30,horario:{lunes:"17:00",martes:"17:30",miercoles:"17:30",jueves:"17:30",viernes:"17:30",sabado:"17:30"}},
-                 {codigo:"XY",Nombre:"algrebra",grupo:"XY",cupo:30,horario:{lunes:"17:00",martes:"17:30",miercoles:"17:30",jueves:"17:30",viernes:"17:30",sabado:"17:30"}},
-                 {codigo:"XZ",Nombre:"algrebra",grupo:"XZ",cupo:30,horario:{lunes:"17:00",martes:"17:30",miercoles:"17:30",jueves:"17:30",viernes:"17:30",sabado:"17:30"}}
-               ]
-             ],
-             [
-             "Lupita",
-             [
-               {codigo:"XA",Nombre:"algrebra",grupo:"X",cupo:30,horario:{lunes:"17:00",martes:"17:30",miercoles:"17:30",jueves:"17:30",viernes:"17:30",sabado:"17:30"}},
-               {codigo:"XA",Nombre:"algrebra",grupo:"XY",cupo:30,horario:{lunes:"17:00",martes:"17:30",miercoles:"17:30",jueves:"17:30",viernes:"17:30",sabado:"17:30"}},
-               {codigo:"XA",Nombre:"algrebra",grupo:"XZ",cupo:30,horario:{lunes:"17:00",martes:"17:30",miercoles:"17:30",jueves:"17:30",viernes:"17:30",sabado:"17:30"}}
-             ]
-           ]
-
-
+              {    nombre:"Martin",materias:[
+                   {codigo:"XZ",nombre:"algrebra",grupo:"XZ",cupo:30,horario:{lunes:{salon:"sc5",hora:"20:30"},martes:{salon:"sc5",hora:"20:30"},miercoles:{salon:"sc5",hora:"20:30"},jueves:{salon:"sc5",hora:"20:30"},viernes:{salon:"sc5",hora:"20:30"},sabado:{salon:"sc5",hora:"20:30"}}},
+                   {codigo:"XY",nombre:"algrebra2",grupo:"XY",cupo:30,horario:{lunes:{salon:"sc5",hora:"20:30"},martes:{salon:"sc5",hora:"20:30"},miercoles:{salon:"sc5",hora:"20:30"},jueves:{salon:"sc5",hora:"20:30"},viernes:{salon:"sc5",hora:"20:30"},sabado:{salon:"sc5",hora:"20:30"}}}
+                                            ]
+               },
+               {    nombre:"Vero",materias:[
+                    {codigo:"XZ",nombre:"algrebra",grupo:"XZ",cupo:30,horario:{lunes:{salon:"sc3",hora:"20:31"},martes:{salon:"sc5",hora:"20:50"},miercoles:{salon:"sc5",hora:"14:30"},jueves:{salon:"sc5",hora:"20:30"},viernes:{salon:"sc5",hora:"20:30"},sabado:{salon:"sc5",hora:"20:30"}}},
+                    {codigo:"X",nombre:"algrebra2",grupo:"XY",cupo:30,horario:{lunes:{salon:"sc3",hora:"20:30"},martes:{salon:"sc5",hora:"20:30"},miercoles:{salon:"sc5",hora:"20:30"},jueves:{salon:"sc5",hora:"20:30"},viernes:{salon:"sc5",hora:"20:30"},sabado:{salon:"sc5",hora:"20:30"}}}
+                                             ]
+                },
+                {    nombre:"Krak",materias:[
+                     {codigo:"AAA",nombre:"algrebra",grupo:"XZ",cupo:30,horario:{lunes:{salon:"sc5",hora:"20:30"},martes:{salon:"sc5",hora:"20:30"},miercoles:{salon:"sc5",hora:"20:30"},jueves:{salon:"sc5",hora:"20:30"},viernes:{salon:"sc5",hora:"20:30"},sabado:{salon:"sc5",hora:"20:30"}}},
+                     {codigo:"XY",nombre:"algrebra2",grupo:"XY",cupo:30,horario:{lunes:{salon:"sc5",hora:"20:30"},martes:{salon:"sc5",hora:"20:30"},miercoles:{salon:"sc5",hora:"20:30"},jueves:{salon:"sc5",hora:"20:30"},viernes:{salon:"sc5",hora:"20:30"},sabado:{salon:"sc5",hora:"20:30"}}}
+                                              ]
+                 }
              ];
-
 
 
 
@@ -233,26 +221,35 @@ jueves:"17:30",viernes:"17:30",sabado:"17:30"}}
 ];
 gpsDisponibles = [];
 materiasSeleccionadas=[];
-materiaModal="Kevin";
+materiaModal="";
   modals = "modal";
   constructor() {
     this.modals = "modal";
   }
   apilarSeleccion($gpsdisp){
-     let temporal={Gpo:$gpsdisp.Gpo,Prof:this.materiaModal+"-"+$gpsdisp.Prof,Hor:$gpsdisp.Hor,Lug:$gpsdisp.Lug};
-
+     let temporal={Gpo:$gpsdisp.Gpo,Prof:this.materiaModal+"-"+$gpsdisp.Prof,Hor:$gpsdisp.Hor,Lug:$gpsdisp.Lug,creditos:$gpsdisp.creditos};
     this.materiasSeleccionadas.push(temporal);
     this.cerrar();
   }
   funcMostrarGrupos($materia) {
-    this.modals = "visibleNo";
-    for (let i = 0; i < this.maestros.length; i++) {
-            for (let j = 0; j < this.maestros[i][1].length; j++) {
 
-              if( this.maestros[i][1][j].codigo == $materia.codigo ){
-                  window.alert(this.maestros[i][1][j].horario.lunes);
-               this.gpsDisponibles.push({Gpo:this.maestros[i][1][j].grupo,Prof:this.maestros[i][0],Hor:this.maestros[i][1][j].horario.lunes,Lug:this.maestros[i][1][j].cupo});
-            }
+    this.gpsDisponibles = [];
+    this.modals = "visibleNo";
+    this.materiaModal=$materia.nombre;
+    for (let i = 0; i < this.maestros.length; i++) {
+            for (let j = 0; j < this.maestros[i].materias.length; j++) {
+                    console.log( this.maestros[i].materias[j].codigo);
+                    if( this.maestros[i].materias[j].codigo == $materia.codigo ){
+                    this.gpsDisponibles.push(
+                    {Gpo:this.maestros[i].materias[j].grupo,
+                     Prof:this.maestros[i].nombre,
+                     Hor:this.maestros[i].materias[j].horario,
+                     Lug:this.maestros[i].materias[j].cupo,
+                     creditos:$materia.creditos}
+
+                   );
+
+                      }
 
              }
       }
@@ -266,7 +263,7 @@ materiaModal="Kevin";
   }
   ngOnInit() {
     this.recuperarSemestre();
-    $(".submenu").click(function() {
+    $(".submenuSeleccionMaterias").click(function() {
       $(this).children("ul").slideToggle();
 
     }
@@ -279,15 +276,12 @@ materiaModal="Kevin";
     if ($(window).width() > 720) {
 
 
-      $(".submenu").children("ul").show();
+      $(".submenuSeleccionMaterias").children("ul").show();
 
 
 
     }
-    else {
-
     }
 
 
-}
 }
