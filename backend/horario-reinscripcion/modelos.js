a34b22b7c99260487a84e9b579f2bfafd558b37e
Alejandro Marrufo Quintana <14041296@itdurango.edu.mx>
2018-11-25 11:41:10 -0600

    modelo empezado

--- backend/horario-reinscripcion/modelos.js
+++ backend/horario-reinscripcion/modelos.js
@@ -1,1 +1,20 @@
 ////aquí va alumno
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema;
+
+var horarioSchema = new mongoose.Schema({
+    // my props
+    nombres: String,
+    apellidos: String,
+    fecha: Date,
+    hora: TimeRanges,
+    autorizado: String,
+    biblioteca: String,
+    financieros: String,
+    encuesta: String    
+});
+
+module.exports =
+ mongoose.model('HorarioReinscripcion',
+  horarioSchema);
+  
