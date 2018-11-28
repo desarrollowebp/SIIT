be832f1ba8c6e6ab0de15bf867d0a8ef0ef9fb90
Alejandro Marrufo Quintana <14041296@itdurango.edu.mx>
2018-11-27 19:28:57 -0600

    ruta le falta

--- backend/horario-reinscripcion/rutas.js
+++ backend/horario-reinscripcion/rutas.js
@@ -1,2 +1,20 @@
 //aqui va alumnos
 const { Router } = require('express')
+
+const {
+    horarioReinscripcion,
+    adeudosHorarioAlumnos
+  } = require('./modelos.js')
+
+  
+const horarioReinscripcionRouter = Router()
+const adeudosHorarioAlumnosRouter = Router()
+
+/**
+ * datos de tabla horario 
+ */
+horarioReinscripcionRouter.get('/', (req, res, next) => {
+    horarioReinscripcion.find()
+      .then(datos => res.json({ datos }))
+      .catch(next)
+  })
