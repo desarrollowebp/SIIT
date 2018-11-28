b13ddc887fe1ea8be4e7b48ae7a885c942a67db8
Alejandro Marrufo Quintana <14041296@itdurango.edu.mx>
2018-11-27 20:07:59 -0600

    extra index, rutas hR

--- backend/horario-reinscripcion/rutas.js
+++ backend/horario-reinscripcion/rutas.js
@@ -14,7 +14,11 @@ const adeudosHorarioAlumnosRouter = Router()
  * datos de tabla horario 
  */
 horarioReinscripcionRouter.get('/', (req, res, next) => {
+    response.render('index'); 
+  /*
     horarioReinscripcion.find()
       .then(datos => res.json({ datos }))
-      .catch(next)
-  })
+      .catch(next)*/
+  });
+
+  ////tengo que cambiar los nombres de el front de la tabla para que concuerde
