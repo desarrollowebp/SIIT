b949cc9ffd7643bec59a164cdfc308dd4837c71a
Alejandro Marrufo Quintana <14041296@itdurango.edu.mx>
2018-11-28 19:50:51 -0600

    modelo extraescolar

--- backend/extraescolar/modelos.js
+++ backend/extraescolar/modelos.js
@@ -1,1 +1,15 @@
-//// dalia puedes ver esto?
+const mongoose = require('mongoose')
+
+const extraescolarSchema =  new mongoose.Schema({
+  periodo: Number,
+  grupo: Number,
+  promotor: String,
+  hInicial: TimeRanges,
+  hFinal: TimeRanges,
+  dias: String,
+  lugares: Number
+})
+
+module.exports = {
+  extraescolar: mongoose.model('extraescolar', extraescolarSchema)
+}
