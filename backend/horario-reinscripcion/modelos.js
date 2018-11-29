540e0a85efbb58804197aadbb167a6a833f4a274
Alejandro Marrufo Quintana <14041296@itdurango.edu.mx>
2018-11-28 18:43:16 -0600

    no se necesita apellido

--- backend/horario-reinscripcion/modelos.js
+++ backend/horario-reinscripcion/modelos.js
@@ -5,20 +5,19 @@ var mongoose = require('mongoose')
 var horarioSchema = new mongoose.Schema({
     // my props
     nombres: String,
-    apellidos: String,
     fecha: Date,
     hora: TimeRanges,
     autorizado: String,
     biblioteca: String,
     financieros: String,
-    encuesta: String    
+    encuesta: String
 });
 
 const adeudosHorarioAlumnoSchema = new mongoose.Schema({
   numeroControl: Number,
   datoshorarioReinscripcion: Boolean
 })
-  
+
 module.exports = {
   horarioReinscripcion: mongoose.model('horarioReinscripcion', horarioSchema),
   adeudosHorarioAlumnos: mongoose.model('adeudosHorarioAlumnos', adeudoshorarioAlumnoSchema)
