49e0256d133b656491db50d9670d7d2a51f446d7
Ricardo Abrego Salmeron <14040428@itdurango.edu.mx>
2018-11-30 12:10:40 -0600

    14040428
    index.js

---
+++ backend/preficha-pago/index.js
@@ -0,0 +1,20 @@
+
+const {
+  PrefichaPago
+} = require('./modelos.js')
+
+const {
+  PrefichaPagoRouter
+} = require('./rutas.js')
+
+module.exports = {
+  /**
+   * Modelos
+   */
+  PrefichaPago,
+
+  /**
+   * Rutas
+   */
+  PrefichaPagoRouter
+}
