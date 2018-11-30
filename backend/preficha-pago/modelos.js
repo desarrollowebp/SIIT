12d277914dd395d9663e5d9fda6ddec2ceeb7114
juan carlos <karlosrt_cobal@hotmail.com>
2018-11-30 14:26:10 -0600

    juan carlos cabrl
    rutas back

--- backend/preficha-pago/modelos.js
+++ backend/preficha-pago/modelos.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 
 const PrefichaPagoSchema = new mongoose.Schema({
+
   nombres: String,
   apellidos: String,
-  numeroControl: Number
+  numeroControl: Number,
+  semestre:Number,
+  periodo:Number,
+  importe:Number,
+  carrera:String,
+  consepto:String,
+  fechavencimiento:Number,
+  sucursal-cuenta:Number,
+  referencia-alfanumerica:Number,
+  fechavencimiento:Number,
+
 });
 
 module.exports = mongoose.model('Alumno', AlumnoSchema);
--- backend/preficha-pago/rutas.js
+++ backend/preficha-pago/rutas.js
@@ -3,5 +3,7 @@ const { Router } = require('express')
 const {
   PrefichaPago
 } = require('./modelos.js')
+const FichasPago = new 
 
 const PrefichaPagoRouter = Router()
+PrefichaPagoRouter.get()
