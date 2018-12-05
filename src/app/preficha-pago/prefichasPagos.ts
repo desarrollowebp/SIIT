ebf86979dda98f9da25619467c793b86d322bf09
Ricardo Abrego Salmeron <14040428@itdurango.edu.mx>
2018-12-04 22:52:14 -0600

    14040428
    prefichaspagos.ts para el frond

---
+++ src/app/preficha-pago/prefichasPagos.ts
@@ -0,0 +1,17 @@
+import { prefichaPago } from './prefichaPago'
+
+export class prefichasPagos {
+  preguntas:PreguntaEvaluacion[]
+
+  nombres:prefichaPago[]
+  apellidos:prefichaPago[]
+  numeroControl:prefichaPago[]
+  semestre:prefichaPago[]
+  periodo:prefichaPago[]
+  importe:prefichaPago[]
+  carrera:prefichaPago[]
+  consepto:prefichaPago[],
+  fechavencimiento:prefichaPago[]
+  sucursalCuenta:prefichaPago[]
+  referenciaAlfanumerica:prefichaPago[]
+}
