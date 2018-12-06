9d46c671d027bc709da6903f17b32b800dbeb5a2
KateVBR <14041418@itdurango.edu.mx>
2018-12-06 14:03:06 -0600

    Backend

--- backend/residencias/index.js
+++ backend/residencias/index.js
@@ -1,21 +1,17 @@
 const {
-<<<<<<< HEAD
+
   datosResidencias,
   respuestasResidencias
-=======
-  datosResidencia,
-  respuestasResidencia,
->>>>>>> 03403cc655d7ea17f03749234b42fc02b10a79ef
+
   
 } = require('./modelos.js')
 
 const {
   datosResidenciasRouter,
-<<<<<<< HEAD
+
   respuestasResidenciasRouter
-=======
   respuestasResidenciasRouter,
->>>>>>> 03403cc655d7ea17f03749234b42fc02b10a79ef
+
 
 } = require('./rutas.js')
 
@@ -23,24 +19,20 @@ module.exports = {
   /**
    * Modelos
    */
-<<<<<<< HEAD
   datosResidencias,
   respuestasResidencias
-=======
+
   datosEvaluaciones,
   respuestasEvaluaciones,
->>>>>>> 03403cc655d7ea17f03749234b42fc02b10a79ef
+
   
 
   /**
    * Rutas
    */
-<<<<<<< HEAD
+
   datosResidenciasRouter,
   respuestasResidenciasRouter
   
-=======
-  datosEvaluacionesRouter,
-  respuestasEvaluacionesRouter
->>>>>>> 03403cc655d7ea17f03749234b42fc02b10a79ef
+
 }
