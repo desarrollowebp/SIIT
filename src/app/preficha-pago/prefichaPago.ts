dd6059b9d848c1b76539467d521d4d24061146c6
Edgar Alexis Mercado Valdez <15040440@itdurango.edu.mx>
2018-12-06 10:12:51 -0600

    arreglo de , por ; en preficha de pago.ts

--- src/app/preficha-pago/prefichaPago.ts
+++ src/app/preficha-pago/prefichaPago.ts
@@ -1,13 +1,13 @@
 export class PrefichaPago {
-  nombres: String,
-  apellidos: String,
-  numeroControl: Number,
-  semestre:Number,
-  periodo:Number,
-  importe:Number,
-  carrera:String,
-  consepto:String,
-  fechavencimiento:Number,
-  sucursalCuenta:Number,
-  referenciaAlfanumerica:Number,
+  nombres: String;
+  apellidos: String;
+  numeroControl: Number;
+  semestre:Number;
+  periodo:Number;
+  importe:Number;
+  carrera:String;
+  consepto:String;
+  fechavencimiento:Number;
+  sucursalCuenta:Number;
+  referenciaAlfanumerica:Number;
 }
