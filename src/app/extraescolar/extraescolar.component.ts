2be9e58099fcd3a117ab413578c962e317b38a4a
Edgar Alexis Mercado Valdez <15040440@itdurango.edu.mx>
2018-12-06 00:02:40 -0600

    depuracion de codigo en el ts

--- src/app/extraescolar/extraescolar.component.ts
+++ src/app/extraescolar/extraescolar.component.ts
@@ -64,9 +64,7 @@ materiaModal="";
                      lugares:this.grupos[i].actividad[j].lugares
                      }
                    );
-
                       }
-
              }
       }
       console.log(this.gpsDisponibles);
@@ -96,9 +94,8 @@ materiaModal="";
    //console.log(this.grupos);
    });
 
-
+    //Regresa todas las actividades extraescolares
    service.getExtraescolares().subscribe((data)=>{
-     //Regresa todas las actividades extraescolares
     this.extras=data;
     this.extras=this.extras.actividadesExtra;
     //Actividades Culturales
@@ -148,64 +145,6 @@ materiaModal="";
    this.service.getGruposDisponibles;
 
   }
-
-  
-  
-
-
-/*
-  bailes = [
-    'Ballet Tahitiano', 'Bellydance Fusión', 'Break Dance', 'Danza Aérea',
-    'Danza Árabe', 'Electro Dance', 'Hip Hop', 'Hip Hop Avanzado', 'Hip Hop Principiante',
-    'Jazz', 'Jazz Avanzado', 'Jazz Principiante', 'Popping Dance', 'Ritmos Latinos',
-    'Ritmos Urbanos', 'Tango', 'Danza Floklorica',
-  ]
-
-  musicales = [
-    'Orquesta Filarmónica', 'Orquesta Sinfónica', 'Rondalla', 'Rondalla Femenil', 'Rondalla Varonil',
-    'Rondalla Y Taller De Guitarra', 'Taller Guitarra Popular', 'Coro De Cámara',
-  ]
-
-  civicas = [
-    'Banda De Guerra', 'Edecanes', 'Escolta y guión', 'Grupo Cívico', 'Oratoria',
-  ]
-
-  dts = [
-    'Periodismo', 'Radio', 'Taller De Arte Literario', 'Teatro', 'Teatro y Pantomima',
-  ]
-
-  visuales = [
-    'Taller De Cinematografía', 'Taller de Fotografía y Dibujo', 'Cine Y Cortometraje',
-  ]
-
-  pelotas = [
-    'Balón Mano', 'Balón Mano Femenil', 'Basquetbol Femenil',
-    'Basquetbol Varonil', 'Beisbol', 'Frontón A Mano',
-    'Fútbol Americano', 'Fútbol Rapido', 'Fútbol Rápido Femenil', 'Fútbol Soccer Varonil',
-    'Hand Ball', 'Hockey Sobre Pasto', 'Ping Pong De Mesa', 'Softbol', 'Tenis', 'Tenis De Mesa',
-    'Tochito', 'Tochito Femenil', 'Voleibol De Playa', 'Voleibol De Playa Femenil',
-    'Voleibol De Playa Varonil', 'Voleibol Femenil', 'Voleibol Varonil', 'Fortalecimiento Y Tonicidad En Frontenis',
-  ]
-
-  fisicas = [
-    'Activacion Fisica', 'Actividad Fisica', 'Aero Figth', 'Aerobics', 'Animacion Deportiva', 'Artes Marciales Europeas',
-    'Atletismo Femenil', 'Atletismo Varonil', 'Box', 'Gimnasia Artística', 'Hiit Workout', 'Insanity', 'Judo', 'Karate Do',
-    'Pesas', 'Porristas', 'Tae Kwon Do', 'Yoga', 'Yoga Dinamico', 'Yoga Iyengar', 'Yoga Meditativo', 'Zumba',
-  ]
-
-  nataciones = [
-    'Natacion', 'Natacion Varonil', 'Natacion Femenil',
-  ]
-
-
-  aeas = [
-    'Ajedrez', 'Ajedrez Paralelo', 'Deportes Electronicos', 'Tiro con Arco',
-  ]
-*/
-
-
-
-  
 
 }
 
