641fa6b8e81d8bd2465a7186ffe974c7d2d88e26
Rene <reidisy@gmail.com>
2018-12-07 02:44:51 -0600

    o

--- backend/app.js
+++ backend/app.js
@@ -12,10 +12,10 @@ const {
     respuestasEvaluacionesRouter,
     adeudosAlumnosRouter
 } = require('./evaluacion-docente')
-///
+    ///
 const {
-  informacionTutoriasRouter
-} = require('./tutorias')  
+    informacionTutoriasRouter
+} = require('./tutorias')
 
 const {
     gruposCargadosRouter
@@ -33,37 +33,27 @@ const {
     ///
 const {
     actividadesRouter,
-<<<<<<< HEAD
-    maestrosExtraescolarRouter
+    maestrosExtraescolarRouter,
+    extraescolarSeleccionadaRouter
 } = require('./extraescolar')
-=======
-      maestrosExtraescolarRouter,
-      extraescolarSeleccionadaRouter
-  } = require('./extraescolar')
->>>>>>> af8ca343a96eb8406181db602cdb9a508a70b62c
 
-  const {
-       calificacionexamenRouter
-    } = require('./calificaciones-examenes')
+const {
+    calificacionexamenRouter
+} = require('./calificaciones-examenes')
 
-  const { 
-      calificacionesParcialesRouter
+const {
+    calificacionesParcialesRouter
 } = require('./calificaciones-parciales')
 
-  const {
+const {
 
 
 
 
-      becaRouter
-  } = require('./verificacion-beca')
+    becaRouter
+} = require('./verificacion-beca')
 const app = express();
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-    next();
-});
+
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -76,39 +66,21 @@ app.use('/alumnos', alumnosRouter);
 app.use('/preguntas-evaluaciones', preguntasEvaluacionesRouter)
 app.use('/respuestas-evaluaciones', respuestasEvaluacionesRouter)
 app.use('/adeudos-alumnos', adeudosAlumnosRouter)
-<<<<<<< HEAD
+    ///
+app.use('/tutorias', informacionTutoriasRouter)
 app.use('/seleccion-materias', semestresMateriaRouter)
 app.use('/seleccion-materias-maestros', maestrosMateriaRouter)
+app.use('/horario-reinscripcion', horarioReinscripcionRouter)
 app.use('/extraescolar', actividadesRouter)
 app.use('/extraescolar-maestros', maestrosExtraescolarRouter)
-    /**************
-     *   Auditoria servicios
-     */
-app.use("/api", require('./auditoria-servicio/rutas'));
-
-///
-app.use('/horario-reinscripcion', horarioReinscripcionRouter)
-    ///
-
-=======
-///
-app.use('/tutorias', informacionTutoriasRouter)
-app.use('/seleccion-materias', semestresMateriaRouter )
-app.use('/seleccion-materias-maestros',  maestrosMateriaRouter)
-app.use('/horario-reinscripcion', horarioReinscripcionRouter)
-app.use('/extraescolar',  actividadesRouter)
-app.use('/extraescolar-maestros',  maestrosExtraescolarRouter)
-app.use('/extraescolar-seleccionada',  extraescolarSeleccionadaRouter)
-app.use('/grupos-cargados',  gruposCargadosRouter)
+app.use('/extraescolar-seleccionada', extraescolarSeleccionadaRouter)
+app.use('/grupos-cargados', gruposCargadosRouter)
 app.use('/calificaciones-parciales', calificacionesParcialesRouter)
 app.use('/calificaciones-examenes', calificacionexamenRouter)
 app.use('/verificion-beca', becaRouter)
-/**************
- *   Auditoria servicios
- */
-app.use(require('./auditoria-servicio/rutas'));
 
->>>>>>> af8ca343a96eb8406181db602cdb9a508a70b62c
+app.use("/api", require('./auditoria-servicio/rutas'));
+
 app.listen(3000, function() {
     console.info('Backend escuchando en el puerto 3000');
 });
