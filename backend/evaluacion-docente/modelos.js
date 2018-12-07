830f994506cbe543712b92bd6b7c154a69921ee0
Orlando Valverde <13041580@itdurango.edu.mx>
2018-12-07 16:55:06 -0600

    se creo clase tipo maestro materia | jairo 12040369

--- backend/evaluacion-docente/modelos.js
+++ backend/evaluacion-docente/modelos.js
@@ -3,22 +3,19 @@ const mongoose = require('mongoose')
 const preguntasEvaluacionesSchema =  new mongoose.Schema({
   pregunta: String
 })
-
 const respuestasEvaluacionesSchema = new mongoose.Schema({
   materia: String,
   respuestas: [{
     type: String
   }]
 })
-
 const adeudosAlumnosSchema = new mongoose.Schema({
   numeroControl: Number,
   evaluacionDocente: Boolean
 })
-
 const maestrosMateriasSchema = new mongoose.Schema({
   maestro: String,
-  materia:String
+  materia: String
 })
 
 module.exports = {
