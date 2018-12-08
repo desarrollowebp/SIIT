81ebc7e61921cada264597508dab6ee2595cd9e7
KateVBR <14041418@itdurango.edu.mx>
2018-12-07 19:01:25 -0600

    cambios

--- src/app/residencias/residencias.service.ts
+++ src/app/residencias/residencias.service.ts
@@ -1,7 +1,7 @@
-import {injectable} from '@angular/core';
+import { Injectable } from '@angular/core'; 
 import{HttpClient} from '@angular/common/http';
 
-@Injectable ()
+@Injectable () 
 export class residenciasService{
 	constructor(private http: HttpClient){ }
 
